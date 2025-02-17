7e8252bb5cacf98ba7d48ad65297aa290dcb2612
viniciuscguedes <viniciuscguedesbsi@gmail.com>
2025-02-17 18:36:12 -0300

    Removendo js desnecessarios

--- src/js/voltaMenu.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const botao = document.getElementById("voltarMenu");
-    if (botao) {
-        botao.addEventListener("click", function () {
-            window.location.href = "../html/index.html";
-        });
-    }
-});
